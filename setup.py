2c80ecc107a7e4cd888d60d4c9ba0434398f200b
Evan Bender <evan.bender@percolate.com>
2015-10-07 22:19:10 -0400

    be bold

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup(
     name='dripconfig',
-    version='0.2.dev1',
+    version='0.2.0',
     description="configuration loading utilities for common process setup tasks",
     license="",
     author="Luke Tucker, James O'Beirne",
