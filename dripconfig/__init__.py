ec1450f8345d9f5380e24382aaf6d0283d43a98e
Luke Tucker <luke@percolate.com>
2015-10-21 11:52:48 -0700

    add a syslog handler that correctly emits the process name

--- dripconfig/__init__.py
+++ dripconfig/__init__.py
@@ -3,9 +3,26 @@ the default 'global' configuration if desired.
 """
 
 from .configdict import ConfigDict
-from .sources import *
-from interfaces import ConfigurationTrigger, ToBeInjected
+from .sources import (
+    Argv,
+    EnvVar,
+    Filename,
+)
+from .helpers import SysLogHandler
+from .interfaces import ConfigurationTrigger, ToBeInjected
+
+__all__ = [
+    'Argv',
+    'ConfigDict',
+    'ConfigurationTrigger',
+    'EnvVar',
+    'Filename',
+    'SysLogHandler',
+    'ToBeInjected',
+    'config'
+]
+
 
 config = ConfigDict()
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
--- dripconfig/helpers.py
+++ dripconfig/helpers.py
@@ -1,5 +1,7 @@
-import json
+from logging.handlers import SysLogHandler as _SysLogHandler
 from dripconfig.interfaces import ConfigurationTrigger
+import os
+import sys
 
 class SchemaTrigger(ConfigurationTrigger):
     """
@@ -15,3 +17,20 @@ class SchemaTrigger(ConfigurationTrigger):
 
     def configure(self, config):
         pass
+
+class SysLogHandler(_SysLogHandler):
+    """
+    A logging.handlers.SyslogHandler that
+    uses the process name as the 'ident'
+    field for messages emitted to syslog (as
+    is common for processes to do...)
+
+    By default, messages logged
+    using logging.handlers.SyslogHandler do not
+    have an ident set leading to some difficultly
+    determining the originating process.
+    """
+
+    def emit(self, record):
+        record.ident = os.path.basename(sys.argv[0])
+        super(SysLogHandler, self).emit(record)
